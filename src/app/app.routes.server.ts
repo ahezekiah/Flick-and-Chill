3a841b089b15230170c6260be6715b906767353e
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2026-09-04 12:39:45 -0600

    Build Should Work Now

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,33 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Dynamic routes
+  {
+    path: 'movie/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'tv/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'person/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:type/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'genres/movie/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'genres/tv/:id',
+    renderMode: RenderMode.Server
+  },
+
+  // Static routes
   {
     path: '**',
     renderMode: RenderMode.Prerender
